a7eebfe81672b59b0f0b8da078b4858367d215d1
cvYouTian <youclark00@gmail.com>
2023-12-04 12:24:30 +0800

    add C3convlution for Lenet5

--- model/LeNet5.py
+++ model/LeNet5.py
@@ -20,11 +20,13 @@ class SubSampling(nn.Module):
     def __init__(self,
                  filter_size: Union[int, tuple, list],
                  stride: Union[int, tuple, list],
-                 padding: Union[int, tuple, list]):
+                 padding: Union[int, tuple, list],
+                 dilation: Union[int, tuple, list]):
 
         super(SubSampling, self).__init__()
         self.stride = stride
         self.padding = [padding, padding] if isinstance(padding, int) else padding
+        self.dilation = [dilation, dilation] if isinstance(dilation, int) else dilation
         self.weight = [filter_size, filter_size] if isinstance(filter_size, int) else filter_size
         self.coefficient = nn.Parameter(torch.ones(1, requires_grad=True))
         self.bias = nn.Parameter(torch.ones(1, requires_grad=True))
@@ -32,10 +34,9 @@ class SubSampling(nn.Module):
     def forward(self, x):
         # BCHW
         b, c, h, w = x.shape
-        out_h = (h + 2 * self.padding[0] - (self.weight[0] - 1) - 1) // self.stride + 1
-        out_w = (w + 2 * self.padding[1] - (self.weight[1] - 1) - 1) // self.stride + 1
-        out_feat = torch.zeros([b, c, out_h, out_w])
-
+        out_h = (h + 2 * self.padding[0] - self.dilation[0] * (self.weight[0] - 1) - 1) // self.stride + 1
+        out_w = (w + 2 * self.padding[1] - self.dilation[1] * (self.weight[1] - 1) - 1) // self.stride + 1
+        out_feat = torch.zeros([b, c, out_h, out_w], dtype=x.dtype, device=x.device)
         for m in range(b):
             for n in range(c):
                 for i in range(out_h):
@@ -50,24 +51,61 @@ class SubSampling(nn.Module):
 
         return out_feat
 
+class C3Convlution(nn.Module):
+
+    def __init__(self,
+                 custom_feat: list,
+                 in_chanel: int,
+                 out_chanel: int,
+                 kernel_size: Union[int, tuple],
+                 stride: Union[int, tuple, list],
+                 padding: Union[int, tuple, list]):
+        super(C3Convlution, self).__init__()
+        self.custom_feat = custom_feat
+        self.in_chanel = in_chanel
+        self.out_chanel = out_chanel
+        self.kernel_size = [kernel_size, kernel_size] if isinstance(kernel_size, int) else kernel_size
+        self.stride = [stride, stride] if isinstance(stride, int) else stride
+        self.padding = [padding, padding] if isinstance(padding, int) else padding
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        feat_container = list()
+        feat_idx = dict()
+        for i in range(b):
+            for idx, j in enumerate(range(c)):
+                feat_idx[idx] = x[i, j, :, :]
+                if idx == len(c) - 1:
+                    feat_container.append(feat_idx)
+
+        for feat in feat_container:
+            for i in self.custom_feat:
+                ...
+
 
 class Lenet5(nn.Module):
     def __init__(self):
         super(Lenet5, self).__init__()
         # [b, 1, 32, 32]-> [1, 6, 28, 28]
         self.C1 = nn.Conv2d(1, 6, 5, 1, 0)
-        self.S2 = SubSampling(2, 2, 0)
+        self.S2 = SubSampling(2, 2, 0, 1)
+        self.C3 = nn.Conv2d(6, 16, 5, )
+
 
 
 if __name__ == "__main__":
 
     beta = torch.randn([5, 3, 40, 40])
-    mypooling = SubSampling(2, 2, 0)
+    mypooling = SubSampling(2, 2, 0, 1)
+    maxp = nn.MaxPool2d(2, 2)
 
     a = mypooling(beta)
-    # print(a.equal(b))
+    b = maxp(beta)
+    print(a.eq(b))
+    # print(a.shape, b.shape)
+    # print(a, b)
 
-    print(a)
+    # print(a)
     #
     # optimizer = torch.optim.SGD([{"params": net.parameters()},
     #                        {"params": my_module.parameters()}],
