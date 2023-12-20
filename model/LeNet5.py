74f330015f0557ffddaa44e12897e1296821a499
cvYouTian <youclark00@gmail.com>
2023-12-20 22:42:43 +0800

    modified C3convlution for Lenet5

--- model/LeNet5.py
+++ model/LeNet5.py
@@ -20,22 +20,21 @@ class SubSampling(nn.Module):
     def __init__(self,
                  filter_size: Union[int, tuple, list],
                  stride: Union[int, tuple, list],
-                 padding: Union[int, tuple, list],
-                 dilation: Union[int, tuple, list]):
+                 padding: Union[int, tuple, list]):
 
         super(SubSampling, self).__init__()
         self.stride = stride
         self.padding = [padding, padding] if isinstance(padding, int) else padding
-        self.dilation = [dilation, dilation] if isinstance(dilation, int) else dilation
         self.weight = [filter_size, filter_size] if isinstance(filter_size, int) else filter_size
         self.coefficient = nn.Parameter(torch.ones(1, requires_grad=True))
         self.bias = nn.Parameter(torch.ones(1, requires_grad=True))
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         # BCHW
         b, c, h, w = x.shape
-        out_h = (h + 2 * self.padding[0] - self.dilation[0] * (self.weight[0] - 1) - 1) // self.stride + 1
-        out_w = (w + 2 * self.padding[1] - self.dilation[1] * (self.weight[1] - 1) - 1) // self.stride + 1
+        out_h = (h + 2 * self.padding[0] - self.weight[0]) // self.stride + 1
+        out_w = (w + 2 * self.padding[1] - self.weight[1]) // self.stride + 1
         out_feat = torch.zeros([b, c, out_h, out_w], dtype=x.dtype, device=x.device)
         for m in range(b):
             for n in range(c):
@@ -46,15 +45,26 @@ class SubSampling(nn.Module):
                         start_height = i * self.stride
                         end_height = start_height + self.stride
                         # this subsample for lenet5
-                        out_feat[m, n, i, j] = self.coefficient*torch.sum(
-                            x[m, n, start_height:end_height, start_width:end_width]) + self.bias
+                        out_feat[m, n, i, j] = self.sigmoid(self.coefficient*torch.sum(
+                            x[m, n, start_height:end_height, start_width:end_width]) + self.bias)
 
         return out_feat
 
-class C3Convlution(nn.Module):
 
+class C3Convlution(nn.Module):
+    """
+    C3's the layer of the convolution in LeNet paper.(Gradient-based learning applied to document recognition)
+
+    Args:
+        custom_feat(dict):
+        in_chanel(int):
+        out_chanel: int
+        kernel_size: Union[int, tuple],
+        stride(int| tuple| list):
+        padding(int | tuple | list): not used in LeNet
+    """
     def __init__(self,
-                 custom_feat: list,
+                 custom_feat: dict,
                  in_chanel: int,
                  out_chanel: int,
                  kernel_size: Union[int, tuple],
@@ -86,27 +96,57 @@ class C3Convlution(nn.Module):
 class Lenet5(nn.Module):
     def __init__(self):
         super(Lenet5, self).__init__()
-        # [b, 1, 32, 32]-> [1, 6, 28, 28]
+        # [b, 1, 32, 32]-> [b, 6, 28, 28]
         self.C1 = nn.Conv2d(1, 6, 5, 1, 0)
-        self.S2 = SubSampling(2, 2, 0, 1)
-        self.C3 = nn.Conv2d(6, 16, 5, )
+        # [b, 6, 14, 14]
+        self.S2 = SubSampling(2, 2, 0)
+        # [b, 16, 10, 10]
+        self.C3 = C3Convlution({0: [0, 1, 2],
+                                1: [1, 2, 3],
+                                2: [2, 3, 4],
+                                3: [3, 4, 5],
+                                4: [0, 4, 5],
+                                5: [0, 1, 5],
+                                6: [0, 1, 2, 3],
+                                7: [1, 2, 3, 4],
+                                8: [2, 3, 4, 5],
+                                9: [0, 3, 4, 5],
+                                10: [0, 1, 4, 5],
+                                11: [0, 1, 2, 5],
+                                12: [0, 1, 3, 4],
+                                13: [1, 2, 4, 5],
+                                14: [0, 2, 3, 5],
+                                15: [0, 1, 2, 3, 4, 5]},
+                               6,
+                               16,
+                               5,
+                                1,
+                               0)
+        # [b, 16, 5, 5]
+        self.S4 = SubSampling(2, 2, 0)
+        # [b, 120, 1, 1]
+        #  a method similar to global pooling
+        self.C5 = nn.Conv2d(16, 120, 5, 1, 0)
+        self.C6 = nn.Linear(120, 84)
+
+    def forward(self, x):
+        x = self.C1(x)
+        x = self.S2(x)
+        x = self.C3(x)
+        x = self.S4(x)
+        logits = self.C5(x)
+
+        return logits
+
 
 
 
 if __name__ == "__main__":
 
     beta = torch.randn([5, 3, 40, 40])
-    mypooling = SubSampling(2, 2, 0, 1)
+    mypooling = SubSampling(2, 2, 0)
     maxp = nn.MaxPool2d(2, 2)
 
     a = mypooling(beta)
     b = maxp(beta)
     print(a.eq(b))
-    # print(a.shape, b.shape)
-    # print(a, b)
-
-    # print(a)
-    #
-    # optimizer = torch.optim.SGD([{"params": net.parameters()},
-    #                        {"params": my_module.parameters()}],
-    #                         lr=base_lr, momentum=momentum, weight_decay=weight_decay)
