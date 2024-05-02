23880dfa7c209cfece270776972a158c100ec0cc
cvYouTian <youclark00@gmail.com>
2024-05-02 20:46:34 +0800

    modified completely predict.py

---
+++ ResNet/predict.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+from pathlib import Path
+import torch
+from PIL import Image
+from torchvision import transforms
+from ResNet34 import ResNet34
+
+
+if __name__ == '__main__':
+    image_path = Path("/home/youtian/Documents/images.jpg")
+    img = Image.open(image_path)
+    img.show()
+
+    trans = transforms.Compose([transforms.Resize(224),
+                                transforms.ToTensor()])
+
+    img = trans(img)
+    img = img.unsqueeze(0)
+    checkpoint = torch.load("/home/youtian/Documents/pro/pyCode/Classic-classification-model/ResNet/runs/Resnet34_30.pt")
+
+    net = ResNet34(num_class=5)
+    net.eval()
+    net.load_state_dict(checkpoint)
+    out = net(img)
+
+    _, preds = out.max(1)
+    classes = {0: "daisy", 1: "dandelion", 2: "rose", 3: "sunflower",4: "tulip"}
+    print(classes[preds.item()])
